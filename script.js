e27affc0bd4991b6ee9826aee39981fd9b13d57d
GodieBar <109578344+GodieBar@users.noreply.github.com>
2023-10-22 05:27:10 -0500

    a

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ function toggleMenu(event) {
     mainNav.classList.toggle('active');
     menuToggle.classList.toggle('active');
 
-    // Obtén las coordenadas del clic
-    const x = event.clientX;
-    const y = event.clientY;
+    // Comprueba si el menú está abierto
+    const isOpen = mainNav.classList.contains('active');
 
-    // Ajusta la posición del menú desplegable
-    mainNav.style.left = x + 'px';
-    mainNav.style.top = y + 'px';
+    if (isOpen) {
+        // Obtén las coordenadas del botón de hamburguesa
+        const rect = menuToggle.getBoundingClientRect();
+
+        // Posiciona el menú desplegable al lado del botón de hamburguesa
+        mainNav.style.left = rect.right + 'px';
+        mainNav.style.top = rect.top + 'px';
+    }
 }
 
 // Obtener el botón de hamburguesa
@@ -20,13 +24,13 @@ const menuToggle = document.querySelector('.menu-toggle');
 
 // Agregar un evento clic al botón de hamburguesa
 menuToggle.addEventListener('click', (event) => {
-    toggleMenu(event); // Abre el menú
+    toggleMenu(event); // Abre o cierra el menú
 });
 
 // Cerrar el menú cuando se hace clic en cualquier parte del documento
 document.addEventListener('click', (event) => {
-    if (!menuToggle.contains(event.target) && !mainNav.contains(event.target)) {
-        mainNav.classList.remove('active');
+    if (!menuToggle.contains(event.target) && !document.querySelector('.main-nav').contains(event.target)) {
+        document.querySelector('.main-nav').classList.remove('active');
         menuToggle.classList.remove('active');
     }
 });
