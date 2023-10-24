b89d215f46246e712b431e24a808e652440b2a53
GodieBar <109578344+GodieBar@users.noreply.github.com>
2023-10-23 23:11:06 -0500

    a

---
+++ Pagina/Eventos/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var dropdown = document.getElementById("myDropdown");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
