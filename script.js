5bdc07f99384df45119283f607f124a9e7e14d02
GodieBar <109578344+GodieBar@users.noreply.github.com>
2023-11-07 07:55:24 -0500

    gg

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-// Función para alternar la visibilidad del menú desplegable
+// Esta función maneja la apertura y el cierre del menú desplegable
 function toggleMenu() {
+    // Seleccionamos el menú desplegable por su ID
     var dropdown = document.getElementById("myDropdown");
-    // Cambia la clase 'active' en el botón del menú de hamburguesa
-    var menuToggle = document.querySelector('.menu-toggle');
-    menuToggle.classList.toggle('active');
 
-    // Alternar la visualización del menú desplegable
+    // Verificamos si el menú ya está visible
     if (dropdown.style.display === "block") {
+        // Si está visible, lo ocultamos
         dropdown.style.display = "none";
     } else {
+        // Si está oculto, lo mostramos
         dropdown.style.display = "block";
     }
 }
 
-// Función para cerrar el modal
+// Esta función cierra el modal cuando se hace clic en el botón de cierre (X)
 function cerrarModal() {
+    // Seleccionamos el modal por su ID
     var modal = document.getElementById("myModal");
-    if (modal) {
-        modal.style.display = "none";
-    }
+    
+    // Establecemos la propiedad de visualización en 'none' para ocultarlo
+    modal.style.display = "none";
 }
 
-// Evento que se dispara cuando el usuario hace clic fuera del menú desplegable
+// Este evento se dispara cuando se hace clic en cualquier lugar fuera del menú desplegable
 window.onclick = function(event) {
+    // Obtenemos el menú desplegable por su ID
     var dropdown = document.getElementById("myDropdown");
+
+    // Comprobamos si se hace clic fuera del menú desplegable
     if (!event.target.matches('.menu-toggle')) {
+        // Si el menú está abierto y se hace clic fuera de él, lo cerramos
         if (dropdown.style.display === "block") {
             dropdown.style.display = "none";
-            document.querySelector('.menu-toggle').classList.remove('active');
         }
     }
 }
 
-// Asegurarse de que el JavaScript no se ejecute hasta que el documento esté completamente cargado
+// Esperamos a que el contenido de la página se cargue completamente
 document.addEventListener("DOMContentLoaded", function () {
-    // Puede poner aquí más código que debe ejecutarse una vez que el DOM esté completamente cargado
+    // Aquí puedes añadir cualquier script que quieras ejecutar después de que el DOM esté completamente cargado
 });
