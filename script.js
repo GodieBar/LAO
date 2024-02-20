2280a3fc2704f6159c8bfbb2bd443efc77647e50
GodieBar <godiebarbosaparra@gmail.com>
2024-02-20 07:53:37 -0500

    PAGINA FINAL FASE 2

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
-// Esta función maneja la apertura y el cierre del menú desplegable
-function toggleMenu() {
-    // Seleccionamos el menú desplegable por su ID
-    var dropdown = document.getElementById("myDropdown");
+document.addEventListener("DOMContentLoaded", function () {
+    var stopAnimationImage = document.querySelector('.stop-animation');
 
-    // Verificamos si el menú ya está visible
-    if (dropdown.style.display === "block") {
-        // Si está visible, lo ocultamos
-        dropdown.style.display = "none";
-    } else {
-        // Si está oculto, lo mostramos
-        dropdown.style.display = "block";
-    }
-}
+    stopAnimationImage.onload = function () {
+        setTimeout(function () {
+            stopAnimationImage.style.animation = 'none'; // Elimina la animación
+        }, 4000);
+    };
 
-// Esta función cierra el modal cuando se hace clic en el botón de cierre (X)
-function cerrarModal() {
-    // Seleccionamos el modal por su ID
-    var modal = document.getElementById("myModal");
+    // Esta función maneja la apertura y el cierre del menú desplegable
+    function toggleMenu() {
+        var dropdown = document.getElementById("myDropdown");
 
-    // Establecemos la propiedad de visualización en 'none' para ocultarlo
-    modal.style.display = "none";
-}
-
-// Este evento se dispara cuando se hace clic en cualquier lugar fuera del menú desplegable
-window.onclick = function(event) {
-    // Obtenemos el menú desplegable por su ID
-    var dropdown = document.getElementById("myDropdown");
-
-    // Comprobamos si se hace clic fuera del menú desplegable
-    if (!event.target.matches('.menu-toggle')) {
-        // Si el menú está abierto y se hace clic fuera de él, lo cerramos
         if (dropdown.style.display === "block") {
             dropdown.style.display = "none";
+        } else {
+            dropdown.style.display = "block";
         }
     }
-};
 
-// Esperamos a que el contenido de la página se cargue completamente
-document.addEventListener("DOMContentLoaded", function () {
-    // Aquí puedes añadir cualquier script que quieras ejecutar después de que el DOM esté completamente cargado
+    // Esta función cierra el modal cuando se hace clic en el botón de cierre (X)
+    function cerrarModal() {
+        var modal = document.getElementById("myModal");
+        modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        var dropdown = document.getElementById("myDropdown");
+
+        if (!event.target.matches('.menu-toggle')) {
+            if (dropdown.style.display === "block") {
+                dropdown.style.display = "none";
+            }
+        }
+    };
 });
